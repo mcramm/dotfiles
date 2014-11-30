9d8f660e763597af041fbdb0ef0f4e56de206c17
Michael Cramm <gmcramm@gmail.com>
2014-11-30 16:16:42 -0700

    Add setup for 'bin', fix indentation

--- setup.sh
+++ setup.sh
@@ -15,54 +15,57 @@ link_to() {
 }
 
 update_submodules() {
-	heading "Updating submodules"
-	git submodule update --init --recursive
+  heading "Updating submodules"
+  git submodule update --init --recursive
 }
 
 link_file() {
-	file=$1
-	dest=$2
-	echo "Linking file ${file} to ${dest}"
-	ln -s "${PWD}/${file}" ${dest}
+  file=$1
+  dest=$2
+  echo "Linking file ${file} to ${dest}"
+  ln -s "${PWD}/${file}" ${dest}
 }
 
 replace_file() {
-	file=$1
-	dest=$2
-	echo "Removing ${dest}"
-	rm -rf ${dest}
-	link_file $file $dest
+  file=$1
+  dest=$2
+  echo "Removing ${dest}"
+  rm -rf ${dest}
+  link_file $file $dest
 }
 
 link_files() {
-	for file in tmux.conf oh-my-zsh gitconfig githelpers zshrc vim vimrc gvimrc inputrc config
-	do
-		dest="${HOME}/.${file}"
+  for file in tmux.conf oh-my-zsh gitconfig githelpers zshrc vim vimrc gvimrc inputrc config
+  do
+    dest="${HOME}/.${file}"
 
-		if [ "${file}" = "vimrc" ]; then
-			file="vim/vimrc"
-		fi
-		if [ "${file}" = "gvimrc" ]; then
-			file="vim/gvimrc"
-		fi
+    if [ "${file}" = "vimrc" ]; then
+      file="vim/vimrc"
+    fi
+    if [ "${file}" = "gvimrc" ]; then
+      file="vim/gvimrc"
+    fi
+    if [ "${file}" = "bin" ]; then
+      dest="${HOME}/${file}"
+    fi
 
 
-		if [ -e $dest ]; then
-			if diff ${dest} ${file} > /dev/null ; then
-				echo "Identcal file at ${dest}. Doing nothing."
-			else
-				echo "File exists at ${dest}."
-				echo "Overwrite? [y/N]"
-				read ANSWER
+    if [ -e $dest ]; then
+      if diff ${dest} ${file} > /dev/null ; then
+        echo "Identcal file at ${dest}. Doing nothing."
+      else
+        echo "File exists at ${dest}."
+        echo "Overwrite? [y/N]"
+        read ANSWER
 
-				if [ "${ANSWER}" = "y" ]; then
-					replace_file $file $dest
-				fi
-			fi
-		else
-			link_file $file $dest
-		fi
-	done
+        if [ "${ANSWER}" = "y" ]; then
+          replace_file $file $dest
+        fi
+      fi
+    else
+      link_file $file $dest
+    fi
+  done
 }
 
 install_homebrew() {
