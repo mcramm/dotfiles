cb1c2eca7b977aa1a93c2dfed28b9575bca58ada
Michael Cramm <mike@petrofeed.com>
2015-09-30 16:19:18 -0600

    Add colorscheme change, some plugin adjustments and boring config stuff

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -6,10 +6,14 @@ function prepend_to_path -d "Prepend the given dir to PATH if it exists and is n
     end
 end
 
+
 set -gx RBENV_ROOT /Users/michaelcramm/.rbenv
 . (rbenv init -|psub)
 
 set -gx ANDROID_HOME /Users/michaelcramm/projects/android/sdk
+set -gX JAVA_HOME (/usr/libexec/java_home)
+
+fish_vi_mode
 
 prepend_to_path "/usr/local/heroku/bin"
 prepend_to_path "/usr/local/sbin"
@@ -17,3 +21,5 @@ prepend_to_path "/usr/local/Cellar/postgresql/9.3.4/bin"
 prepend_to_path "/Applications/Postgres.app/Contents/Versions/9.3/bin"
 prepend_to_path "/usr/local/bin"
 prepend_to_path "$HOME/bin"
+
+eval sh "$HOME/Downloads/base16-shell/base16-default.dark.sh"
